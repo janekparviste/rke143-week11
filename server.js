98932441e07c99ee37e8ecc7a03d695ead1bccaa
JanekParviste <janek.parviste@tktk.ee>
2024-10-03 17:09:12 +0300

    rke143 week 10 code

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const recipeRouter = require('./routes/recipes.routes');
 const ingredientRouter = require('./routes/ingredients.routes');
+const fullRecipesRouter = require('./routes/fullRecipes.routes');
 const app = express();
 
 app.use('/ingredients', ingredientRouter);
 
 app.use('/recipes', recipeRouter);
+
+app.use('/fullrecipes', fullRecipesRouter);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
