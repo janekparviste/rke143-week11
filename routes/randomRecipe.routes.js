2412fb102357d16f79d45bf8437d858264f1efc0
JanekParviste <janek.parviste@tktk.ee>
2024-10-13 12:16:06 +0300

    adde imageURL column

--- routes/randomRecipe.routes.js
+++ routes/randomRecipe.routes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const recipeQuery =
-        'SELECT id, recipeName, instructions FROM recipe ORDER BY RANDOM() LIMIT 1;';
+        'SELECT id, recipeName, imageURL, instructions FROM recipe ORDER BY RANDOM() LIMIT 1;';
 
     const recipeResult = await db.query(recipeQuery);
     selectedRecipe = recipeResult.rows[0];
